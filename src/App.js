dc27c4481c57b329d7cf95e492f0abd28e06bbc2
lloyd64 <122186067+lloyd64@users.noreply.github.com>
2023-02-09 12:45:01 -0500

    lecard->phillies

--- src/App.js
+++ src/App.js
@@ -5,22 +5,30 @@ function App() {
   return (
 
 <>
-    <div class="outline">
-      <div class="lecard">
-        <h2>Some Nerd</h2>
-        <img src="https://i.kym-cdn.com/news_feeds/icons/mobile/000/037/359/05b.jpg" alt="Obi-Wan headshot" />
-        <p class="details">Hello there <br></br> Lightsaber go <i>brrrr</i></p>
-      </div>
-    </div>
-  <div class="buttons">
-  
-  <button class="copyCat">Copy</button>
-  <button id="del">Delete</button>
-  <button class="btnDet">Toggle Details</button> 
-  <button class="btnCT">Change Title</button>
-  <button  class="btnBG">Change Background Color</button>
-  
-  </div>
+<div class="buttons">
+
+<button class="duplicate">DUPLICATE</button>
+
+<button onclick="changeHeading()" class="change">CHANGE HEADING</button>
+
+<button class="bg-color">CHANGE COLOR</button>
+
+<button onclick="deleteLast()" class="delete">DELETE COPY</button>
+</div>
+
+<div class="card">
+<img class ="img" src="https://content.sportslogos.net/logos/54/70/full/hplu542ts3fp99gkmueu7uirg.gif"/>
+
+<h1 class="h1">NLCS Game 5: Phillies vs Padres</h1>
+
+<img class="bryce" src = "https://a57.foxnews.com/static.foxnews.com/foxnews.com/content/uploads/2022/10/1440/810/bryce-hr.jpg?ve=1&tl=1"/>
+ <p class="description">Seconds after Bryce Harper's 8th inning go ahead home run in the 2022 NLCS</p>
+<a>
+<button class ="details">DETAILS</button>
+</a>
+</div>
+
+
   
 </>
   );
